97bdd3e04cfcc60d327793087bbbbda6e083d38d
Joseph Chiang <josephj6802@gmail.com>
2024-05-10 06:14:27 +1000

    Fixes #34 [Acknowledgements] Ability to search

--- src/pages/Content/acknowledgement-list/acknowledgement-list.tsx
+++ src/pages/Content/acknowledgement-list/acknowledgement-list.tsx
@@ -1,16 +1,23 @@
 import {
   Badge,
+  Button,
   FormLabel,
   Grid,
   GridItem,
+  Input,
+  InputGroup,
+  InputRightElement,
   Link,
+  Highlight,
   HStack,
+  Stack,
   Switch,
   Text,
 } from '@chakra-ui/react';
-import React, {useEffect, useState} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faXmark} from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react';
+import debounce from 'debounce';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import {
   useAcknowledgementAddMutation,
   useAcknowledgementRemoveMutation,
@@ -22,25 +29,41 @@ import {
 } from '@generated/ignition/types';
 
 const ACCESSED_ACKNOWLEDGEMENTS_LOG = 'accessed_acknowledgements';
+const ACKNOWLEDGEMENTS_SEARCH_KEYWORD = 'acknowledgements_search_keyword';
+
+const updateLocalStorage = debounce((key: string, value: string) => {
+  chrome.storage.local.set({
+    [key]: value,
+  });
+}, 500);
 
 export const AcknowledgementList = () => {
-  const {data, loading, refetch} = useAcknowledgementsQuery();
+  const { data, loading, refetch } = useAcknowledgementsQuery();
   const [acks, setAcks] = useState<Acknowledgement[]>([]);
-  const [acknowledgementRemove, {loading: isProcessingRemoveAck}] =
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [acknowledgementRemove, { loading: isProcessingRemoveAck }] =
     useAcknowledgementRemoveMutation();
-  const [acknowledgementAdd, {loading: isProcessingAddAck}] =
+  const [acknowledgementAdd, { loading: isProcessingAddAck }] =
     useAcknowledgementAddMutation();
+
+  useEffect(() => {
+    chrome.storage.local.get([ACKNOWLEDGEMENTS_SEARCH_KEYWORD], (result) => {
+      if (result[ACKNOWLEDGEMENTS_SEARCH_KEYWORD]) {
+        setSearchKeyword(result[ACKNOWLEDGEMENTS_SEARCH_KEYWORD]);
+      }
+    });
+  }, []);
 
   useEffect(() => {
     if (!data) {
       return;
     }
-    const {acknowledgements = []} = data;
+    const { acknowledgements = [] } = data;
 
     chrome.storage.local.get([ACCESSED_ACKNOWLEDGEMENTS_LOG], (result) => {
       const savedAcks = result[ACCESSED_ACKNOWLEDGEMENTS_LOG] || [];
       const newAcks = acknowledgements.filter(
-        ({id}) => !savedAcks.some((ack: Acknowledgement) => (ack.id === id))
+        ({ id }) => !savedAcks.some((ack: Acknowledgement) => ack.id === id)
       );
       if (newAcks.length) {
         const nextAcks = [...savedAcks, ...newAcks];
@@ -62,9 +85,9 @@ export const AcknowledgementList = () => {
     checked: boolean
   ) => {
     if (!checked) {
-      await acknowledgementRemove({variables: {id, level}});
+      await acknowledgementRemove({ variables: { id, level } });
     } else {
-      await acknowledgementAdd({variables: {id, level}});
+      await acknowledgementAdd({ variables: { id, level } });
     }
     await refetch();
   };
@@ -83,8 +106,20 @@ export const AcknowledgementList = () => {
     setAcks(nextAcks);
   };
 
+  const handleChangeSearchKeyword = (event: React.SyntheticEvent) => {
+    const { value } = event.target as HTMLInputElement;
+    setSearchKeyword(value);
+
+    updateLocalStorage(ACKNOWLEDGEMENTS_SEARCH_KEYWORD, value);
+  };
+
+  const handleReset = () => {
+    setSearchKeyword('');
+    updateLocalStorage(ACKNOWLEDGEMENTS_SEARCH_KEYWORD, '');
+  };
+
   const acknowledgements = acks
-    .map(({id, ...rest}) => {
+    .map(({ id, ...rest }) => {
       const matched = data.acknowledgements.find((ack) => ack.id === id);
       return {
         id,
@@ -93,50 +128,71 @@ export const AcknowledgementList = () => {
         isEnabled: !!matched,
       };
     })
+    .filter(({ id }) =>
+      searchKeyword.trim() ? id.includes(searchKeyword.trim()) : true
+    )
     .sort((a, b) => (a.id > b.id ? 1 : -1));
 
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-      {acknowledgements?.map(({id, isEnabled, level, updatedAt}) => (
-        <GridItem
-          borderRadius="md"
-          borderColor="gray.200"
-          borderStyle="solid"
-          borderWidth="1px"
-          padding="large"
-          key={id}
-        >
-          <HStack display="flex" width="100%">
-            <Switch
-              size="lg"
-              defaultChecked={isEnabled}
-              isDisabled={isProcessingRemoveAck || isProcessingAddAck}
-              onChange={(e) => handleToggleAck(id, level, e.target.checked)}
-              {...{id}}
-            />
-            <FormLabel m={0} htmlFor={id} flex="1">
-              <Text>{id}</Text>
-              {updatedAt ? (
-                <Text fontSize="sm" color="gray.500">
-                  {updatedAt}
-                </Text>
-              ) : null}
-            </FormLabel>
-            <Badge
-              colorScheme={
-                level === AcknowledgementLevel.USER ? 'green' : 'yellow'
-              }
-              size="small"
-              textTransform="lowercase"
-            >
-              {level}
-            </Badge>
-            <Link colorScheme="blue" onClick={() => handleRemove(id)}>
-              <FontAwesomeIcon icon={faXmark}/>
-            </Link>
-          </HStack>
-        </GridItem>
-      ))}
-    </Grid>
+    <Stack spacing="large">
+      <InputGroup size="md">
+        <Input
+          pr="4.5rem"
+          placeholder="Search acknowledgement..."
+          value={searchKeyword}
+          onChange={handleChangeSearchKeyword}
+        />
+        <InputRightElement width="4.5rem">
+          <Button onClick={handleReset}>Reset</Button>
+        </InputRightElement>
+      </InputGroup>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        {acknowledgements?.map(({ id, isEnabled, level, updatedAt }) => (
+          <GridItem
+            borderRadius="md"
+            borderColor="gray.200"
+            borderStyle="solid"
+            borderWidth="1px"
+            padding="large"
+            key={id}
+          >
+            <HStack display="flex" width="100%">
+              <Switch
+                size="lg"
+                defaultChecked={isEnabled}
+                isDisabled={isProcessingRemoveAck || isProcessingAddAck}
+                onChange={(e) => handleToggleAck(id, level, e.target.checked)}
+                {...{ id }}
+              />
+              <FormLabel m={0} htmlFor={id} flex="1">
+                <Highlight
+                  query={[searchKeyword.trim()]}
+                  styles={{ fontWeight: 'bold' }}
+                >
+                  {id}
+                </Highlight>
+                {updatedAt ? (
+                  <Text fontSize="sm" color="gray.500">
+                    {updatedAt}
+                  </Text>
+                ) : null}
+              </FormLabel>
+              <Badge
+                colorScheme={
+                  level === AcknowledgementLevel.USER ? 'green' : 'yellow'
+                }
+                size="small"
+                textTransform="lowercase"
+              >
+                {level}
+              </Badge>
+              <Link colorScheme="blue" onClick={() => handleRemove(id)}>
+                <FontAwesomeIcon icon={faXmark} />
+              </Link>
+            </HStack>
+          </GridItem>
+        ))}
+      </Grid>
+    </Stack>
   );
 };
