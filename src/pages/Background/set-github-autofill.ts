f2d77f07f7e58980bdfeee02c7ad2741ce0c8064
Joseph Chiang <josephj6802@gmail.com>
2024-05-30 05:34:18 +1000

    Bug: [GitHub Autofill] Restricting Repo (fixes #8)

--- src/pages/Background/set-github-autofill.ts
+++ src/pages/Background/set-github-autofill.ts
@@ -9,15 +9,10 @@ import { GITHUB_AUTOFILL } from '../Content/lib';
         hostEquals: 'github.com',
         pathPrefix: '/ignitionapp/Practice-Ignition/compare/',
       },
-      {
-        hostEquals: 'github.com',
-        pathPrefix: '/josephj/ignition-toolbelt/compare/',
-      },
     ],
   };
 
   const handleLoadPage = ({ tabId, url }: { tabId: number; url: string }) => {
-    console.log('=>(set-github-autofill.ts:19) url', url);
     chrome.tabs.sendMessage(tabId, {
       type: GITHUB_AUTOFILL,
       value: url,
